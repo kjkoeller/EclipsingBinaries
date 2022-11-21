578843fa028d1120017985505f0a8eb6d8bb81a7
Kyle Koeller <kjkoeller@bsu.edu>
2022-11-21 15:25:06 -0500

    File to automatically setup dependencies
    This file allows the user to say python setup.py install and it will check for a minimum version of python and install all the required dependencies.

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Setup script for the package."""
+
+import os
+import sys
+
+import setuptools
+
+
+MINIMUM_PYTHON_VERSION = 3, 5
+
+
+def check_python_version():
+    """Exit when the Python version is too low."""
+    if sys.version_info < MINIMUM_PYTHON_VERSION:
+        sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
+
+
+check_python_version()
+
+setuptools.setup(
+    description="Binary Star Package for Ball State University's Astronomy Research Group",
+    url='https://github.com/kjkoeller/Binary_Star_Research_Package',
+    author='Kyle Koeller',
+
+    packages=setuptools.find_packages(),
+
+    entry_points={'console_scripts': []},
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    install_requires=open("requirements.txt").readlines(),
+)

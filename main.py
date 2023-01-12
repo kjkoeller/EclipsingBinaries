c1570f95ad3c456699f7e25fe7a74707277fee86
Kyle Koeller <kjkoeller@bsu.edu>
2023-01-12 13:51:33 -0500

    Added O-C Plotting (#34)
    O-C Plotting was not allowed to selected as an option.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
 The main program helps centralize all the other programs into one selection routine that can be run and call all
 other programs.
-
 Author: Kyle Koeller
 Created: 8/29/2022
-Last Updated: 11/10/2022
+Last Updated: 01/12/2023
 """
 
 import tess_data_search
@@ -24,7 +23,7 @@ def main():
     while True:
         print(
             "TESS Database Search/Download(1), AIJ/TESS Night Filters(2), AIJ Comparison Star Selector(3), O'Connel Effect(4), "
-            "Color Light Curve(5), IRAF Reduction(6), Close Program(7)")
+            "Color Light Curve(5), IRAF Reduction(6), O-C Plotting(7), Close Program(8)")
         print("")
         prompt = int(input("Please type out the number corresponding to the corresponding action: "))
         if prompt == 1:
@@ -48,9 +47,9 @@ def main():
         elif prompt == 6:
             IRAF_Reduction.main()
         elif prompt == 7:
-            break
-        elif prompt == 8:
             OC_plot.data_fit()
+        elif prompt == 8:
+            break
         else:
             print("You have not entered any of the allowed entries, please try again.")
             print()

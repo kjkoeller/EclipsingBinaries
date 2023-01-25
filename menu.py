e0fb4ce0d51731345232af4cf28df221b1d31daa
Kyle Koeller <kjkoeller@bsu.edu>
2023-01-25 15:22:46 -0500

    Update and rename main.py to menu.py

--- menu.py
+++ menu.py
@@ -3,16 +3,16 @@ The main program helps centralize all the other programs into one selection rout
 other programs.
 Author: Kyle Koeller
 Created: 8/29/2022
-Last Updated: 01/12/2023
+Last Updated: 01/25/2023
 """
 
-import tess_data_search
-import Night_Filters
-import apass
-import OConnell
-import color_light_curve
-import IRAF_Reduction
-import OC_plot
+from .tess_data_search import main as data_search
+from .Night_Filters import main as night
+from .apass import comparison_selector as comp_select
+from.OConnell import main as oconnell
+from .color_light_curve import color_gui as gui
+from .IRAF_Reduction import main as IRAF
+from .OC_plot import data_fit as data_fit
 
 
 def main():
@@ -26,27 +26,27 @@ def main():
         print("")
         prompt = int(input("Please type out the number corresponding to the corresponding action: "))
         if prompt == 1:
-            tess_data_search.main()
+            data_search()
         elif prompt == 2:
             aij = input("AIJ or TESS or 'Go Back': ")
             if aij.lower() == "aij":
-                Night_Filters.main(0)
+                night(0)
             elif aij.lower() == "tess":
-                Night_Filters.main(1)
+                night(1)
             elif aij.lower() == "go back":
                 pass
             else:
                 print("You did not enter AIJ or TESS please go back through the prompts again and enter AIJ or TESS.")
         elif prompt == 3:
-            apass.comparison_selector()
+            comp_select()
         elif prompt == 4:
-            OConnell.main()
+            oconnell()
         elif prompt == 5:
-            color_light_curve.color_gui(False)
+            gui(False)
         elif prompt == 6:
-            IRAF_Reduction.main()
+            IRAF()
         elif prompt == 7:
-            OC_plot.data_fit()
+            data_fit()
         elif prompt == 8:
             break
         else:

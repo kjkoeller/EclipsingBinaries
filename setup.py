e8041b41d2fa0964b43da1704e9831fe5767f75d
Kyle Koeller <kjkoeller@bsu.edu>
2023-01-26 10:35:55 -0500

    Adding Version Checking
    Added version checking for the end user to be able to type `EclipsingBinaries --version`

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 import sys
+import codecs
+import os.path
 
 from pathlib import Path
 this_directory = Path(__file__).parent
@@ -12,6 +14,18 @@ long_description = (this_directory / "README.md").read_text()
 
 MINIMUM_PYTHON_VERSION = 3, 8
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 def check_python_version():
     """Exit when the Python version is too low."""
@@ -22,7 +36,7 @@ def check_python_version():
 check_python_version()
 
 setup(
-    version="2.1.6",
+    version=get_version("EclipsingBinaries/__init__.py"),
     name="EclipsingBinaries",
     description="Binary Star Package for Ball State University's Astronomy Research Group",
     long_description=long_description,

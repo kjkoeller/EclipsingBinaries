ec9052476ef8bffda6320c9ad8ab909620756ed2
Kyle Koeller <kjkoeller@bsu.edu>
2023-05-14 16:51:22 -0400

    Menu Options and Format (#194)
    Changes the format of the menu to be a pandas dataframe to display the options and corresponding numbers in a table like format. This makes it easier to read than what was displayed beforehand.

--- EclipsingBinaries/menu.py
+++ EclipsingBinaries/menu.py
@@ -3,8 +3,10 @@ The main program helps centralize all the other programs into one selection rout
 other programs.
 Author: Kyle Koeller
 Created: 8/29/2022
-Last Updated: 04/05/2023
+Last Updated: 05/14/2023
 """
+
+import pandas as pd
 
 """
 # testing imports
@@ -15,7 +17,8 @@ from OConnell import main as oconnell
 from color_light_curve import color_gui as gui
 from IRAF_Reduction import main as IRAF
 from OC_plot import main as data_fit
-from gaia import main
+from gaia import target_star as gaia
+from find_min import main as find_min
 """
 from .tess_data_search import main as data_search
 from .Night_Filters import main as night
@@ -25,23 +28,33 @@ from .color_light_curve import color_gui as gui
 from .IRAF_Reduction import main as IRAF
 from .OC_plot import main as data_fit
 from .gaia import target_star as gaia
+from .find_min import main as find_min
 
 
 def main():
     print("If you need a description of what each option does, please refer to the README for this packages GitHub page"
-          " https://github.com/kjkoeller/Binary_Star_Research_Package")
+          " https://github.com/kjkoeller/EclipsingBinaries/")
     print("\nWhich program do you want to run?\n\n")
 
+    options = ["IRAF Reduction", "Find Minimum", "TESS Database Search/Download", "AIJ Comparison Star Selector",
+               "BSUO or SARA/TESS Night Filters", "O-C Plotting", "Gaia Search", "O'Connel Effect", "Color Light Curve",
+               "Close Program"]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    total = pd.DataFrame({"Options": options,
+                          "Numbers": numbers})
+
     while True:
-        print(
-            "IRAF Reduction(1), TESS Database Search/Download(2), AIJ Comparison Star Selector(3), "
-            "BSUO or SARA/TESS Night Filters(4), O-C Plotting(5), Gaia Search(6), O'Connel Effect(7), "
-            "Color Light Curve(8), Close Program(9)\n")
+        print(total.to_string(index=False))
+        print()
         prompt = int(input("Please type out the number corresponding to the corresponding action: "))
-        if prompt == 2:
+        if prompt == 3:
             # tess data
             data_search()
-        elif prompt == 4:
+        elif prompt == 2:
+            # find minimum
+            find_min()
+        elif prompt == 5:
             # night filters for AIJ and TESS
             aij = input("BSUO/SARA or TESS or 'Go Back': ")
             if aij.lower() == "aij":
@@ -52,25 +65,25 @@ def main():
                 pass
             else:
                 print("\nYou did not enter AIJ or TESS please go back through the prompts again and enter AIJ or TESS.\n")
-        elif prompt == 3:
+        elif prompt == 4:
             # comparison finder
             comp_select()
-        elif prompt == 7:
+        elif prompt == 8:
             # o'connell effect
             oconnell()
-        elif prompt == 8:
+        elif prompt == 9:
             # color light curve
             gui(False)
         elif prompt == 1:
             # iraf reduction
             IRAF()
-        elif prompt == 5:
+        elif prompt == 6:
             # O-C plotting
             data_fit()
-        elif prompt == 9:
+        elif prompt == 10:
             # close program
             break
-        elif prompt == 6:
+        elif prompt == 7:
             gaia()
         else:
             print("\nYou have not entered any of the allowed entries, please try again.\n")

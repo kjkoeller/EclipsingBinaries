0b22a8ce7789cc7e7ff8149cde307fd908f0c675
Kyle Koeller <kjkoeller@bsu.edu>
2023-05-16 14:37:38 -0400

    Many Errors Before Calculations
    This fixes many many errors before the calculations of magnitudes even begins. Now to fix the calculations as the values are not even close to correct.

--- EclipsingBinaries/multi_aperture_photometry.py
+++ EclipsingBinaries/multi_aperture_photometry.py
@@ -3,7 +3,7 @@ Analyze images using aperture photometry within Python and not with Astro ImageJ
 
 Author: Kyle Koeller
 Created: 05/07/2023
-Last Updated: 05/14/2023
+Last Updated: 05/16/2023
 """
 
 # Python imports
@@ -12,14 +12,22 @@ import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 import time
+import warnings
 
 # Astropy imports
 import ccdproc as ccdp
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
+# from astropy.nddata import CCDData
 # from astropy.stats import sigma_clipped_stats
-from photutils import CircularAperture, CircularAnnulus, aperture_photometry
+from photutils.aperture import CircularAperture, CircularAnnulus, aperture_photometry
 from astropy.wcs import WCS
+import astropy.units as u
+from astropy import wcs
+
+# turn off this warning that just tells the user,
+# "The warning raised when the contents of the FITS header have been modified to be standards compliant."
+warnings.filterwarnings("ignore", category=wcs.FITSFixedWarning)
 
 
 def main():
@@ -32,19 +40,22 @@ def main():
     science_imagetyp = 'LIGHT'
     images_path = Path(path)
     files = ccdp.ImageFileCollection(images_path)
-    image_list = files.ccds(imagetyp=science_imagetyp, return_fname=True)
+    image_list = files.files_filtered(imagetyp=science_imagetyp)
+    print(type(image_list))
 
-    multi_aperture_photometry(image_list)
+    multi_aperture_photometry(image_list, images_path)
 
 
-def multi_aperture_photometry(image_list):
+def multi_aperture_photometry(image_list, path):
     """
     Perform aperture photometry on a list of images.
 
     Parameters
     ----------
-    image_list : list
-        List of images to perform aperture photometry on.
+    path : path
+        Path to the folder containing the images.
+    image_list : table
+        Table of images to perform aperture photometry on.
 
     Returns
     -------
@@ -60,19 +71,12 @@ def multi_aperture_photometry(image_list):
     gain = 1.43  # * u.electron / u.adu  # gathered from fits headers manually
 
     # Magnitudes of the comparison stars (replace with your values)
-    df = pd.read_csv('254037_B.radec', skiprows=1, delim_whitespace=True)
+    df = pd.read_csv('254037_B.radec', skiprows=7, sep=",", header=None)
     magnitudes_comp = df[4]
     ra = df[0]
     dec = df[1]
     ref_star = df[2]
     # centroid = df[3]  # Not used (I don't think at least)
-
-    # Convert RA and DEC to pixel positions
-    sky_coords = SkyCoord(ra, dec, unit='deg', frame='icrs')
-    pixel_coords = WCS.all_world2pix(sky_coords.ra, sky_coords.dec, 1)
-
-    target_position = pixel_coords[0]
-    comparison_positions = pixel_coords[1:]
 
     magnitudes = []
     mag_err = []
@@ -86,10 +90,20 @@ def multi_aperture_photometry(image_list):
     fig, ax = plt.subplots()
 
     for _, image_file in enumerate(image_list):
-        image_data, header = fits.getdata(image_file, header=True)
+        image_data, header = fits.getdata(path / image_file, header=True)
+        wcs = WCS(header)
 
-        hjd.append(header['HJD'])
-        bjd.append(header['BJD'])
+        # ccd = CCDData(image_data, wcs=wcs, unit='adu')
+
+        # Convert RA and DEC to pixel positions
+        sky_coords = SkyCoord(ra, dec, unit=(u.h, u.deg), frame='icrs')
+        pixel_coords = wcs.world_to_pixel(sky_coords)
+
+        target_position = pixel_coords[0]
+        comparison_positions = pixel_coords[1:]
+
+        hjd.append(header['HJD-OBS'])
+        bjd.append(header['BJD-OBS'])
 
         # Create the apertures and annuli
         target_aperture = CircularAperture(target_position, r=aperture_radius)
@@ -99,31 +113,31 @@ def multi_aperture_photometry(image_list):
 
         # Perform aperture photometry
         target_phot_table = aperture_photometry(image_data, target_aperture)
-        comparison_phot_tables = aperture_photometry(image_data, comparison_apertures)
+        comparison_phot_tables = [aperture_photometry(image_data, aperture) for aperture in comparison_apertures]
 
         # Perform annulus photometry to estimate the background
         target_annulus_table = aperture_photometry(image_data, target_annulus)
-        comparison_annuli_tables = aperture_photometry(image_data, comparison_annuli)
+        comparison_annuli_tables = [aperture_photometry(image_data, annulus) for annulus in comparison_annuli]
 
         # Calculate the background mean and standard deviation
-        target_background_mean = target_annulus_table['aperture_sum'][0] / target_annulus.area()
+        target_background_mean = target_annulus_table['aperture_sum'][0] / target_annulus.area
         target_background_stddev = np.sqrt(target_background_mean)
 
-        comparison_background_means = [table['aperture_sum'][0] / annulus.area()
+        comparison_background_means = [table['aperture_sum'][0] / annulus.area
                                        for table, annulus in zip(comparison_annuli_tables, comparison_annuli)]
         comparison_background_stddevs = [np.sqrt(mean) for mean in comparison_background_means]
 
         # Subtract the background: Calculate net integrated counts
-        target_sum = target_phot_table['aperture_sum'][0] - target_aperture.area() * target_background_mean
-        comparison_sums = [table['aperture_sum'][0] - aperture.area() * mean
+        target_sum = target_phot_table['aperture_sum'][0] - target_aperture.area * target_background_mean
+        comparison_sums = [table['aperture_sum'][0] - aperture.area * mean
                            for table, aperture, mean in
                            zip(comparison_phot_tables, comparison_apertures, comparison_background_means)]
 
         # Calculate the errors
         target_error = np.sqrt(
-            target_sum / gain + target_aperture.area() * target_background_stddev ** 2 + target_aperture.area() ** 2 * read_noise ** 2)
+            target_sum / gain + target_aperture.area * target_background_stddev ** 2 + target_aperture.area ** 2 * read_noise ** 2)
         comparison_errors = [
-            np.sqrt(comp_sums / gain + aperture.area() * stddev ** 2 + aperture.area() ** 2 * read_noise ** 2)
+            np.sqrt(comp_sums / gain + aperture.area * stddev ** 2 + aperture.area ** 2 * read_noise ** 2)
             for comp_sums, aperture, stddev in zip(comparison_sums, comparison_apertures, comparison_background_stddevs)]
 
         # Calculate the magnitude and error of the target star

b92177552e4920f2b7cf017c85fd516400a86366
Kyle Koeller <kjkoeller@bsu.edu>
2023-06-23 18:54:55 -0400

    Update conf.py (#266)

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,45 @@
 # Configuration file for the Sphinx documentation builder.
 
+import datetime
+import os
+import sys
+
+# Get configuration information from setup.cfg
+try:
+    from ConfigParser import ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+conf = ConfigParser()
+
+conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.py')])
+setup_cfg = dict(conf.items('metadata'))
+
+# By default, highlight as Python 3.
+highlight_language = 'python3'
+
+exclude_patterns.append('_templates')
+
 # -- Project information
 
-project = 'EclipsingBinaries'
-copyright = '2023, Kyle Koeller'
-author = 'Kyle Koeller'
+project = setup_cfg['name']
+author = setup_cfg['author']
+copyright = '{0}, {1}'.format(
+    datetime.datetime.now().year, setup_cfg['author'])
 
-release = '2.8'
-version = '2.8.4a8'
+__import__(project)
+package = sys.modules[project]
+
+ver = package.__version__
+version = '.'.join(ver.split('.'))[:5]
+release = ver
+
+html_title = '{0} v{1}'.format(project, release)
 
 # The master toctree document.
 root_doc = 'index'
+
+man_pages = [('index', project.lower(), project + u' Documentation',
+              [author], 1)]
 
 # -- General configuration
 
@@ -36,3 +65,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+github_issues_url = 'https://github.com/astropy/ccdproc/issues/'
+nitpicky = True
+

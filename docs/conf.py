3041f64672bc06f31f560039d4af79c1179a2ff0
Kyle Koeller <kjkoeller@bsu.edu>
2024-02-16 10:11:14 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-# html_theme = "sphinx_rtd_theme"
+html_theme = "classic"
 
 # -- Options for EPUB output
 # epub_show_urls = 'footnote'

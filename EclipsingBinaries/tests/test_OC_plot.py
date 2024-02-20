f1ff086caf9eb04594ada18aa27aa2a83f9f75ae
Kyle Koeller <kjkoeller@bsu.edu>
2024-02-20 13:32:18 -0600

    Remove Unsued Variables
    Removed unused variables from being processed.

--- EclipsingBinaries/tests/test_OC_plot.py
+++ EclipsingBinaries/tests/test_OC_plot.py
@@ -6,20 +6,20 @@ from .OC_plot import calculate_oc, arguments
 
 class TestCalculateOC(unittest.TestCase):
     def test_Calculate_OC1(self):
-        e, OC, OC_err, T0, T0_err = calculate_oc(10, 0.1, 0, 0.1, 1)
+        e, OC, OC_err, _, _ = calculate_oc(10, 0.1, 0, 0.1, 1)
         self.assertEqual(e, 0.5)
         self.assertEqual(OC, '-0.50000')
         self.assertEqual(OC_err, "0.14142")
-
+    
     def test_Calcualte_OC2(self):
-        e, OC, OC_err, T0, T0_err = calculate_oc(15.0, 0.1, 5.0, 0.1, 2.0)
+        e, OC, OC_err, _, _ = calculate_oc(15.0, 0.1, 5.0, 0.1, 2.0)
         self.assertEqual(e, 5.0)
         self.assertEqual(OC, '0.00000')
         self.assertEqual(OC_err, "0.14142")
     
     def test_Calcualte_OC3(self):
         # line 13 of the test_minimums.txt in the examples folder
-        e, OC, OC_err, T0, T0_err = calculate_oc(2458843.932122, 0.00058, 2457143.761819, 0.00014, 0.31297)
+        e, OC, OC_err, _, _ = calculate_oc(2458843.932122, 0.00058, 2457143.761819, 0.00014, 0.31297)
         self.assertEqual(e, 5432.5)
         self.assertEqual(OC, '-0.03922')
         self.assertEqual(OC_err, "0.00060")

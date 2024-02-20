5576e634a9aa119ee93ccef8eba084b858ab9e9e
Kyle Koeller <kjkoeller@bsu.edu>
2024-02-20 13:48:48 -0600

    New Tests for OC_plot (#351)
    * New Tests for OC_plot
    
    Creating new and actual unit tests for `OC_plot.py`
    
    * Remove Unsued Variables
    
    Removed unused variables from being processed.

--- EclipsingBinaries/tests/test_OC_plot.py
+++ EclipsingBinaries/tests/test_OC_plot.py
@@ -1,25 +1,43 @@
-from .OC_plot import calculate_oc
+import unittest
+# from unittest import mock
+from unittest.mock import patch
+from .OC_plot import calculate_oc, arguments
 
 
-def test_calculate_oc():
-    # Test case 1: Test when T0 = 0
-    m = 10.0
-    err = 0.1
-    T0 = 0
-    T0_err = 0.1
-    p = 1.0
-    e, OC, OC_err, T0, T0_err = calculate_oc(m, err, T0, T0_err, p)
-    assert e == 10.0
-    assert OC == "0.00000"
-    assert OC_err == "0.14142"
+class TestCalculateOC(unittest.TestCase):
+    def test_Calculate_OC1(self):
+        e, OC, OC_err, _, _ = calculate_oc(10, 0.1, 0, 0.1, 1)
+        self.assertEqual(e, 0.5)
+        self.assertEqual(OC, '-0.50000')
+        self.assertEqual(OC_err, "0.14142")
+    
+    def test_Calcualte_OC2(self):
+        e, OC, OC_err, _, _ = calculate_oc(15.0, 0.1, 5.0, 0.1, 2.0)
+        self.assertEqual(e, 5.0)
+        self.assertEqual(OC, '0.00000')
+        self.assertEqual(OC_err, "0.14142")
+    
+    def test_Calcualte_OC3(self):
+        # line 13 of the test_minimums.txt in the examples folder
+        e, OC, OC_err, _, _ = calculate_oc(2458843.932122, 0.00058, 2457143.761819, 0.00014, 0.31297)
+        self.assertEqual(e, 5432.5)
+        self.assertEqual(OC, '-0.03922')
+        self.assertEqual(OC_err, "0.00060")
 
-    # Test case 2: Test when T0 != 0
-    m = 15.0
-    err = 0.1
-    T0 = 5.0
-    T0_err = 0.1
-    p = 2.0
-    e, OC, OC_err, T0, T0_err = calculate_oc(m, err, T0, T0_err, p)
-    assert e == 5.0
-    assert OC == "0.00000"
-    assert OC_err == "0.14142"
+class TestArguments(unittest.TestCase):
+    @patch('builtins.input', side_effect=["10.0", "0.02", "0.312"])
+    def test_arguments1(self, mock_inputs):
+        result = arguments()
+        self.assertEqual(result[0], 10)
+        self.assertEqual(result[1], 0.02)
+        self.assertEqual(result[2], 0.312)
+    
+    @patch('builtins.input', side_effect=["100", "0.02", "-0.312"])
+    def test_arguments2(self, mock_inputs):
+        result = arguments()
+        self.assertEqual(result[0], 100.0)
+        self.assertEqual(result[1], 0.02)
+        self.assertEqual(result[2], -0.312)
+
+
+unittest.main()

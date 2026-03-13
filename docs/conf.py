f106ad1ce44f9e4fb126864d470db9700e6b2ff3
Kyle Koeller <kjkoeller@bsu.edu>
2026-03-13 11:14:11 -0400

    Refractor for consolidation and updated requirements

--- docs/conf.py
+++ docs/conf.py
@@ -1,70 +1,46 @@
 # Configuration file for the Sphinx documentation builder.
 
 import datetime
+from configparser import ConfigParser
+from importlib.metadata import version as get_version
 import os
-import sys
 
 # Get configuration information from setup.cfg
-try:
-    from ConfigParser import ConfigParser
-except ImportError:
-    from configparser import ConfigParser
 conf = ConfigParser()
-
 conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.cfg')])
 setup_cfg = dict(conf.items('metadata'))
 
 # By default, highlight as Python 3.
 highlight_language = 'python3'
 
-# exclude_patterns.append('_templates')
-
 # -- Project information
-
 project = setup_cfg['name']
 author = setup_cfg['author']
-copyright = '{0}, {1}'.format(
-    datetime.datetime.now().year, setup_cfg['author'])
+copyright = '{0}, {1}'.format(datetime.datetime.now().year, author)
 
-__import__(project)
-package = sys.modules[project]
-
-ver = package.__version__
-version = '.'.join(ver.split('.'))[:5]
-release = ver
+release = get_version("EclipsingBinaries")
+version = '.'.join(release.split('.')[:2])
 
 html_title = '{0} v{1}'.format(project, release)
 
-# The master toctree document.
+# -- General configuration
 root_doc = 'index'
 
 man_pages = [('index', project.lower(), project + u' Documentation',
               [author], 1)]
 
-# -- General configuration
 extensions = [
+    'sphinx_astropy',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
 # -- Options for HTML output
-html_theme = "classic"
-
-# -- Options for EPUB output
-# epub_show_urls = 'footnote'
-# github_issues_url = 'https://github.com/astropy/ccdproc/issues/'
-# nitpicky = True
-
-
-
+html_theme = "furo"
